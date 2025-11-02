1718c8eda6ebcc72ec7aa11ed7522ae031683f13
Gabriel <gabriel.magnus.wright@gmail.com>
2025-11-02 18:59:58 +0000

    Setup different environment and development appsettings. With different logging levels.

--- UserManagement.Web/Program.cs
+++ UserManagement.Web/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Westwind.AspNetCore.Markdown;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,16 +14,25 @@ builder.Services
     .AddControllersWithViews();
 
 var app = builder.Build();
-
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
 app.UseMarkdown();
 
 app.UseHsts();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 app.UseAuthorization();
 
 app.MapDefaultControllerRoute();
+
+
+if (builder.Environment.IsDevelopment())
+{
+    logger.LogInformation("Running in Development environment");
+}
+else
+{
+    logger.LogInformation("App started"); // production-safe log
+}
 
 app.Run();
